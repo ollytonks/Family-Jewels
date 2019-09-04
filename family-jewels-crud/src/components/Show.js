0a4a8bb433447471ec6fb6d9c6ca9b62fc7b9a42
Oliver Tonks <otonks@student.unimelb.edu.au>
2019-09-04 15:56:14 +1000

    Unified Image upload in line with other components

--- family-jewels-crud/src/components/Show.js
+++ family-jewels-crud/src/components/Show.js
@@ -40,26 +40,26 @@ class Show extends Component {
     render() {
         return (
             <div class="container">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                <h4><Link to="/">Heirloom List</Link></h4>
-                <h3 class="panel-title">
-                    {this.state.board.title}
-                </h3>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    <h4><Link to="/">Heirloom List</Link></h4>
+                    <h3 class="panel-title">
+                        {this.state.board.title}
+                    </h3>
+                    </div>
+                    <div class="panel-body">
+                    <dl>
+                        <dt>Description:</dt>
+                        <dd>{this.state.board.description}</dd>
+                        <dt>Guardian:</dt>
+                        <dd>{this.state.board.author}</dd>
+                        <dt>Next guardian:</dt>
+                        <dd>{this.state.board.nextguardian}</dd>
+                    </dl>
+                    <Link to={`/edit/${this.state.key}`} class="btn btn-success">Edit</Link>&nbsp;
+                    <button onClick={this.delete.bind(this, this.state.key)} class="btn btn-danger">Delete</button>
+                    </div>
                 </div>
-                <div class="panel-body">
-                <dl>
-                    <dt>Description:</dt>
-                    <dd>{this.state.board.description}</dd>
-                    <dt>Guardian:</dt>
-                    <dd>{this.state.board.author}</dd>
-                    <dt>Next guardian:</dt>
-                    <dd>{this.state.board.nextguardian}</dd>
-                </dl>
-                <Link to={`/edit/${this.state.key}`} class="btn btn-success">Edit</Link>&nbsp;
-                <button onClick={this.delete.bind(this, this.state.key)} class="btn btn-danger">Delete</button>
-                </div>
-            </div>
             </div>
         );
     }
