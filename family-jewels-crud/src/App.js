b020bb0cd17ccfeb7dac2c664e1ee30b3a81b460
Oliver Tonks <otonks@student.unimelb.edu.au>
2019-09-04 14:22:07 +1000

    If applied this will create a link on the app homepage to link to a place where can upload and display an image

--- family-jewels-crud/src/App.js
+++ family-jewels-crud/src/App.js
@@ -42,9 +42,12 @@ class App extends Component {
             <h3 class="panel-title">
               BOARD LIST
             </h3>
+            
           </div>
           <div class="panel-body">
             <h4><Link to="/create">Add Board</Link></h4>
+            <br></br>
+            <h4><Link to="/uploadimage">Upload an Image</Link></h4>
             <table class="table table-stripe">
               <thead>
                 <tr>
